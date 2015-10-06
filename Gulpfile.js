9e72c7343151377e25f21a3733d7d5ddf8d10ada
Hollis Lau <hollislau@hotmail.com>
2015-10-06 11:18:34 -0700

    Add utility files and icons to file structure

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,26 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var webpack = require("webpack-stream");
 
 gulp.task("sass", function() {
   gulp.src("sass/**/*.scss")
   .pipe(sass().on("error", sass.logError))
-  .pipe(gulp.dest("public/css"));
+  .pipe(gulp.dest("public/css/"));
 });
 
-gulp.task("default", function() {
+gulp.task('webpack', function(callback) {
+  return gulp.src("js/**/*.js")
+  .pipe(webpack({
+    output: {
+      filename: "main.js"
+    }
+  }))
+  .pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task("default", ["sass", "webpack"], function() {
   gulp.watch("sass/**/*.scss", ["sass"]);
-  // gulp.watch(config)
+  gulp.watch("js/**/*.js", ["webpack"]);
 });
 
 

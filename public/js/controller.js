f98ca8598e84e6f9c6c4cc53e594d97da4197052
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-10-08 10:44:37 -0700

    angular app working with an array of objects.

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,10 +1,15 @@
 console.log('controller here');
 
+posts = [
+  {
+    title: 'Blog post title',
+    body: 'Sed pulvinar magna odio, ut mattis sapien scelerisque vitae. Sed non auctor massa. Vestibulum mollis at augue at scelerisque. Duis et nunc lacus. Ut ornare efficitur justo nec facilisis. Donec pharetra porta sapien ac varius. Ut vehicula scelerisque erat, in cursus quam dapibus sit amet. Maecenas eu quam at dolor convallis maximus.',
+    author: 'Pinky McGee',
+    date: new Date('1971', '02', '01'),
+    comments: 'Lots of comments',
+  }];
+
 blog.controller('controller', ['$scope', function($scope) { 
-  $scope.title = 'Blog post title'
-  $scope.body = 'Here is a sentence. Here is another. Here is a sentence. Here is another. Here is a sentence. Here is another. Here is a sentence. Here is another. Here is a sentence. Here is another. Here is a sentence. Here is another. Here is a sentence. Here is another. Here is a sentence. Here is another. Here is a sentence. Here is another.'; 
-  $scope.author = 'Pinky McGee';
-  $scope.date = 'March 1, 1971';
-  $scope.comments = 'Lots of comments';
+  $scope.firstpost = posts[0];
 }]);
 

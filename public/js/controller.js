e610d056bd6c0504f85a1160154cb679fc142a8e
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-10-08 11:18:54 -0700

    commit changes before merge to resolve conflicts

--- public/js/controller.js
+++ public/js/controller.js
@@ -7,7 +7,15 @@ posts = [
     author: 'Pinky McGee',
     date: new Date('1971', '02', '01'),
     comments: 'Lots of comments',
-  }];
+  }
+    {
+    title: 'The title of another blog post',
+    body: 'Sed pulvinar magna odio, ut mattis sapien scelerisque vitae. Sed non auctor massa. Vestibulum mollis at augue at scelerisque. Duis et nunc lacus. Ut ornare efficitur justo nec facilisis. Donec pharetra porta sapien ac varius. Ut vehicula scelerisque erat, in cursus quam dapibus sit amet. Maecenas eu quam at dolor convallis maximus.',
+    author: 'Phyllis Diller',
+    date: new Date('2015', '09', '08'),
+    comments: 'Not so many comments',
+  }
+];
 
 blog.controller('controller', ['$scope', function($scope) { 
   $scope.firstpost = posts[0];
